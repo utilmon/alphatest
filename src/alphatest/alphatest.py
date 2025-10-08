d6d21501b7a7804d6115fb9f87c126a85cc891b2
utilmon <utilmonn@gmail.com>
2025-10-08 10:48:49 +0900

    draw drawdown

--- src/alphatest/alphatest.py
+++ src/alphatest/alphatest.py
@@ -5,7 +5,7 @@ import numpy as np
 import matplotlib.pyplot as plt
 import matplotlib.dates as mdates
 
-plt.rcParams["figure.figsize"] = (11, 6)
+plt.rcParams["figure.figsize"] = (11, 12)
 
 
 class backtest:
@@ -50,45 +50,43 @@ class backtest:
         return alpha_df.sum(axis=1).mean()
 
     def compute_metrics(self, strategy_return: pd.Series, annual_ticks: int):
+
+        metrics_map = {}
         total_ticks = strategy_return.shape[0]
         cumulative = (strategy_return + 1).prod()
         annual_return = (cumulative ** (annual_ticks / total_ticks)) - 1
         # annual_return = strategy_return.mean() * annual_ticks
+        metrics_map["annual_return"] = annual_return
         annual_vol = np.sqrt(annual_ticks) * strategy_return.std()
-        sharpe = (
+        metrics_map["annual_vol"] = annual_vol
+        metrics_map["sharpe"] = (
             (annual_return - self.risk_free_return) / annual_vol
             if annual_vol != 0
             else np.nan
         )
-        profit_factor = (
+        metrics_map["profit_factor"] = (
             strategy_return[strategy_return > 0].sum()
             / -strategy_return[strategy_return < 0].sum()
         )
         downside_std = strategy_return[strategy_return < 0].std() * np.sqrt(
             annual_ticks
         )
-        sortino = (
+        metrics_map["sortino"] = (
             (annual_return - self.risk_free_return) / downside_std
             if downside_std != 0
             else np.nan
         )
         cum_returns = (strategy_return + 1).cumprod()
         drawdown = cum_returns / cum_returns.cummax() - 1
+        metrics_map["drawdown"] = drawdown
         max_drawdown = drawdown.min()
-        calmar = (
+        metrics_map["max_drawdown"] = max_drawdown
+        metrics_map["calmar"] = (
             (annual_return - self.risk_free_return) / -max_drawdown
             if max_drawdown != 0
             else np.nan
         )
-        return [
-            sharpe,
-            profit_factor,
-            annual_return,
-            max_drawdown,
-            annual_vol,
-            sortino,
-            calmar,
-        ]
+        return metrics_map
 
     def get_optimal_bins(self, array: np.array):
         """
@@ -154,10 +152,22 @@ class backtest:
         )
         turnover = self.compute_turnover(alpha_df)
         strategy_return -= turnover * self.fee
-        metrics = self.compute_metrics(strategy_return, self.annual_ticks)
-        daily_turnover = turnover.mean() * self.daily_ticks
-        long_short = self.compute_position(alpha_df)
-        metrics.extend([daily_turnover * 100, long_short])
+        metrics_map = self.compute_metrics(strategy_return, self.annual_ticks)
+        metrics_map["returns"] = strategy_return
+        metrics_map["turnover"] = turnover.mean() * self.daily_ticks * 100
+        metrics_map["long_short"] = self.compute_position(alpha_df)
+
+        parameters_to_show = [
+            "sharpe",
+            "profit_factor",
+            "annual_return",
+            "max_drawdown",
+            "annual_vol",
+            "sortino",
+            "calmar",
+            "turnover",
+            "long_short",
+        ]
 
         metrics_df = pd.DataFrame(
             {
@@ -172,11 +182,14 @@ class backtest:
                     "Daily Turnover (%)",
                     "Long/Short position",
                 ],
-                "Strategy": [f"{metric:.2f}" for metric in metrics],
+                "Strategy": [
+                    f"{metrics_map[param]:.2f}" for param in parameters_to_show
+                ],
             }
         )
 
         price_map = {}
+        drawdown_map = {}
         strategy_index = strategy_return.index
         start, end = strategy_index[0], strategy_index[-1]
         # strategy_return.plot()
@@ -191,25 +204,36 @@ class backtest:
                 annual_ticks = 252
             elif benchmark in self.crypto_benchmarks:
                 annual_ticks = 365
-            metrics = self.compute_metrics(df, annual_ticks=annual_ticks)
+            bench_metrics = self.compute_metrics(df, annual_ticks=annual_ticks)
+            bench_metrics["turnover"] = 0
+            bench_metrics["long_short"] = 1
 
-            metrics.extend([0, 1])
-            metrics_df[benchmark] = [f"{metric:.2f}" for metric in metrics]
+            metrics_df[benchmark] = [
+                f"{bench_metrics[param]:.2f}" for param in parameters_to_show
+            ]
             price_map[benchmark] = np.cumprod(1 + df)
+            drawdown_map[benchmark] = bench_metrics["drawdown"]
 
         print(metrics_df)
+
+        fig, axs = plt.subplots(nrows=2, ncols=1, gridspec_kw={"height_ratios": [2, 1]})
 
         price_map["strategy"] = np.cumprod(1 + strategy_return)
         df_price = pd.DataFrame(price_map).sort_index()
         # print(df_price)
 
-        ax = df_price.ffill().plot()
-        # date_format = mdates.AutoDateFormatter(mdates.MonthLocator())
-        # ax.xaxis.set_major_formatter(date_format)
-        plt.ylabel("Growth of $1 investment")
-        plt.xlabel("Date")
-        plt.yscale("log")
-        plt.legend()
-        plt.show()
+        df_price.ffill().plot(ax=axs[0])
+        axs[0].set_ylabel("Growth of $1 investment")
+        # axs[0].set_xlabel("Date")
+        axs[0].set_yscale("log")
 
-        return strategy_return
+        drawdown_map["strategy"] = metrics_map["drawdown"]
+        df_drawdown = pd.DataFrame(drawdown_map).sort_index()
+
+        df_drawdown.ffill().plot(ax=axs[1])
+        axs[1].set_ylabel("Drawdown")
+        axs[1].set_xlabel("Date")
+
+        # plt.show()
+
+        return metrics_map
